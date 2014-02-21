19294f34cbdd9924cd117046df91e2d4a354f4fe
Alexander Rusakov <a_s_rusakov@mail.ru>
2014-02-22 02:13:13 +0400

    ready

--- jquery.wheel.js
+++ jquery.wheel.js
@@ -1,86 +1,84 @@
-(function ($, document, undefined) {
-	var jEvent = $.event,
-		addEvent,
-		eventName,
-		handler,
+(function ($, undefined) {
+	var eventName,
 		normalizeEvent;
 
-	// if ( document.onwheel !== undefined || document.documentMode > 8 ) {
-	// 	eventName = "wheel";
-	// 	normalizeEvent = function (event, eventOrigin) {
-	// 		event.deltaMode = eventOrigin.deltaMode;
-	// 		event.deltaX = eventOrigin.deltaX;
-	// 		event.deltaY = eventOrigin.deltaY;
-	// 		event.deltaZ = eventOrigin.deltaZ;
-	// 	};
-	// } else 
-	if ( document.onmousewheel !== undefined ) {
+	if ( document.onwheel !== undefined || document.documentMode > 8 ) {
+		eventName = "wheel";
+		normalizeEvent = function ( event, origin ) {
+			event.deltaMode = origin.deltaMode;
+			event.deltaX = origin.deltaX;
+			event.deltaY = origin.deltaY;
+			event.deltaZ = origin.deltaZ;
+		};
+	} else 	if ( document.onmousewheel !== undefined ) {
 		eventName = "mousewheel";
-		normalizeEvent = function (event, eventOrigin) {
+		normalizeEvent = function ( event, origin ) {
 			event.deltaMode = 0;
 
-			if ( eventOrigin.wheelDeltaY !== undefined ) {
+			if ( origin.wheelDeltaY !== undefined ) {
 				// for WebKit
-				event.deltaX = -e.wheelDeltaX;
-				event.deltaY = -e.wheelDeltaY;
-				event.deltaZ = -e.wheelDeltaZ;
+				event.deltaX = -origin.wheelDeltaX;
+				event.deltaY = -origin.wheelDeltaY;
 			} else {
 				// for older IE
 				event.deltaX = 0;
-				event.deltaY = -e.wheelDelta;
-				event.deltaZ = 0;
+				event.deltaY = -origin.wheelDelta;
 			}
+			event.deltaZ = 0;
 		};
 	} else {
 		// firefox zone
-		var firefoxString = navigator.userAgent.match(/firefox\/\d+\.?\d*/i),
+		var firefoxMatches = navigator.userAgent.match(/firefox\/(\d+\.?\d*)/i),
 			firefoxVersion = 1;
 		
-		if ( firefoxString && firefoxString.length ) {
-			firefoxVersion = +firefoxString[0].substr(8);
+		if ( firefoxMatches ) {
+			firefoxVersion = +firefoxMatches[1];
 		}
 		
 		if ( firefoxVersion > 3.5 ) {
 			eventName = "MozMousePixelScroll";
-			normalizeEvent = function (event, eventOrigin) {
+			normalizeEvent = function ( event, origin ) {
 				event.deltaMode = 0;
 				event.deltaZ = 0;
 				
-				if ( eventOrigin.axis === eventOrigin.VERTICAL_AXIS ) {
-					event.deltaY = eventOrigin.detail;
+				if ( origin.axis === origin.VERTICAL_AXIS ) {
+					event.deltaY = origin.detail;
 					event.deltaX = 0;
-				} else if ( eventOrigin.axis === eventOrigin.HORIZONTAL_AXIS ) {
-					event.deltaX = eventOrigin.detail;
+				} else if ( origin.axis === origin.HORIZONTAL_AXIS ) {
+					event.deltaX = origin.detail;
 					event.deltaY = 0;
 				}
 			};
 		} else {
 			eventName = "DOMMouseScroll";
-			normalizeEvent = function ( event, eventOrigin) {
+			normalizeEvent = function ( event, origin ) {
 				event.deltaMode = 0;
 				event.deltaX = 0;
-				event.deltaY = eventOrigin.detail;
+				event.deltaY = origin.detail;
 				event.deltaZ = 0;
 			}
 		}
 	}
 
-	console.log(eventName);
-	
-	if ( jEvent.fixHooks && jEvent.mouseHooks ) {
-		jEvent.fixHooks[eventName] = {
-			props: jEvent.mouseHooks.props,
+	if ( $.event.fixHooks && $.event.mouseHooks ) {
+		$.event.fixHooks[eventName] = {
+			props: $.event.mouseHooks.props,
 			filter: function ( event, origin ) {
-				event.type = 'wheel';
-				return jEvent.mouseHooks.filter ? jEvent.mouseHooks.filter(event, origin) : event;
+				normalizeEvent( event, origin );
+				return $.event.mouseHooks.filter(event, origin);
 			}
 		}	
 	}
 	
-	
-	jEvent.special.wheel = {
+	$.event.special.wheel = {
 		version: "0.1",
 		bindType: eventName,
 		delegateType: eventName		
 	};
-})( jQuery, document );
+
+	$.event.special[ eventName ] = {
+		preDispatch: function ( event ) {
+			event.type = "wheel";
+		}
+	};
+})( jQuery );
